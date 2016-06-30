391e95b12e35997fcbf0cde33ace615e8dfd7bd9
Bill Decoste <wdecoste@redhat.com>
2016-06-30 09:09:28 -0700

    test

--- app-jee-jsp/src/main/java/org/keycloak/quickstart/appjee/ServiceLocator.java
+++ app-jee-jsp/src/main/java/org/keycloak/quickstart/appjee/ServiceLocator.java
@@ -9,32 +9,30 @@ import java.net.URL;
  */
 public class ServiceLocator {
 
-    public static URL getServiceUrl(HttpServletRequest req) {
+	public static URL getServiceUrl(HttpServletRequest req) {
+		try {
+			String url = new URL(req.getRequestURL().toString()).getHost();
+			System.out.println("!!!!!! url " + url);
+			//if ( host != null && !host.equals("localhost"))
+			//	return new URL("http://" + host + "/service");
+		} catch (MalformedURLException e){
+		}
 
-        String uri = null;
-        try {
-            uri = System.getProperty("service.url");
-            if (uri != null) {
-                return new URL(uri);
-            }
+		String host = req.getLocalAddr();
 
-            uri = System.getenv("SERVICE_URL");
-            if (uri != null) {
-                return new URL(uri);
-            }
+		if (host.equals("localhost")) {
+			try {
+				host = java.net.InetAddress.getLocalHost().getHostAddress();
+			} catch (Exception e) {
+			}
+		}
+		
+		System.out.println("!!!!!! host " + host);
 
-            String host = new URL(req.getRequestURL().toString()).getHost();
-            if (host == null) {
-                host = "localhost";
-            }
-
-            uri = "http://" + host + ":8080/service";
-            return new URL(uri);
-
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("Malformed url: " + uri);
-        } finally {
-            System.out.println("Service url: " + uri);
-        }
-    }
+		try {
+			return new URL("http://" + host + ":8080/service");
+		} catch (MalformedURLException e){
+			throw new RuntimeException("Malformed URL: " + host);
+		}
+	}
 }
