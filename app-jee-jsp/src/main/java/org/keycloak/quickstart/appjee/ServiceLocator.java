3438b42d56c52177fb643d3cecd1dc95d80f5ea4
Bill Decoste <wdecoste@redhat.com>
2016-06-30 09:48:10 -0700

    test

--- app-jee-jsp/src/main/java/org/keycloak/quickstart/appjee/ServiceLocator.java
+++ app-jee-jsp/src/main/java/org/keycloak/quickstart/appjee/ServiceLocator.java
@@ -8,31 +8,40 @@ import java.net.URL;
  * @author <a href="mailto:mstrukel@redhat.com">Marko Strukelj</a>
  */
 public class ServiceLocator {
+	
+	private static final String SERVICE_URI_INIT_PARAM_NAME = "serviceUrl";
 
 	public static URL getServiceUrl(HttpServletRequest req) {
-		try {
-			String url = new URL(req.getRequestURL().toString()).getHost();
-			System.out.println("!!!!!! url " + url);
-			//if ( host != null && !host.equals("localhost"))
-			//	return new URL("http://" + host + "/service");
-		} catch (MalformedURLException e){
-		}
-
-		String host = req.getLocalAddr();
-
-		if (host.equals("localhost")) {
-			try {
-				host = java.net.InetAddress.getLocalHost().getHostAddress();
-			} catch (Exception e) {
-			}
-		}
+		URL serviceUrl = null;
 		
-		System.out.println("!!!!!! host " + host);
-
 		try {
-			return new URL("http://" + host + ":8080/service");
-		} catch (MalformedURLException e){
-			throw new RuntimeException("Malformed URL: " + host);
+			String url = req.getServletContext().getInitParameter(SERVICE_URI_INIT_PARAM_NAME);
+		    if (url != null && !url.contains("localhost")){
+		    	try {
+					serviceUrl = new URL(url);
+					return serviceUrl;
+				} catch (MalformedURLException e){
+					throw new RuntimeException("Malformed URL: " + url);
+				}
+		    }
+	
+			String host = req.getLocalAddr();
+	
+			if (host.equals("localhost")) {
+				try {
+					host = java.net.InetAddress.getLocalHost().getHostAddress();
+				} catch (Exception e) {
+				}
+			}
+			
+			try {
+				serviceUrl = new URL("http://" + host + ":8080/service");
+				return serviceUrl;
+			} catch (MalformedURLException e){
+				throw new RuntimeException("Malformed URL: " + host);
+			}
+		} finally {
+			System.out.println("Using Service URL " + serviceUrl);
 		}
 	}
 }
